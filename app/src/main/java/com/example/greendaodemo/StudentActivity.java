fb08c631ca1a85084e3aaf014d49163dc3ef83cc
1812821235 <1812821235@qq.com>
2022-08-10 08:19:32 +0800

    数据库操作

--- app/src/main/java/com/example/greendaodemo/StudentActivity.java
+++ app/src/main/java/com/example/greendaodemo/StudentActivity.java
@@ -14,7 +14,10 @@ import com.example.greendaodemo.manager.EventUtils;
 import com.example.greendaodemo.manager.StudentUtils;
 import com.example.greendaodemo.table.EventBeanTable;
 
+import org.greenrobot.greendao.query.DeleteQuery;
+import org.greenrobot.greendao.query.Query;
 import org.greenrobot.greendao.query.QueryBuilder;
+import org.greenrobot.greendao.query.WhereCondition;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -143,7 +146,7 @@ public class StudentActivity extends AppCompatActivity {
     public void onTest11(View v) {
         List<StudentBean> beans = new ArrayList<>();
         for (int i = 0; i < 10; i ++) {
-            StudentBean bean = new StudentBean((long) i, i * 10, "test" + (i % 3), 20 + i, i % 2 == 0 ? "男" : "女");
+            StudentBean bean = new StudentBean((long) i, i, "test" + (i % 3), 20 + i, i % 2 == 0 ? "男" : "女");
             beans.add(bean);
         }
         StudentUtils.insertOrReplaceInTx(beans);
@@ -162,13 +165,82 @@ public class StudentActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 查询uid大于5的学生
+     * @param v
+     */
+    public void onTest13(View v) {
+        QueryBuilder qb = StudentUtils.queryBuilder();
+        Query<StudentBean> query = qb.where(new WhereCondition.StringCondition("UID IN " +
+                        "(SELECT UID FROM STUDENT_BEAN WHERE UID > 5)")
+        ).build();
+        List<StudentBean> beans = query.list();
+        for (int i = 0; i < beans.size(); i ++) {
+            log("index " + i + ": " + beans.get(i).toString());
+        }
+    }
 
 
+    /**
+     * uid大于5小于10，Name值为test0的数据
+     * @param v
+     */
+    public void onTest14(View v) {
+        QueryBuilder qb = StudentUtils.queryBuilder();
+        List<StudentBean> beans = qb.where(StudentBeanDao.Properties.Name.eq("test0"),
+                qb.and(StudentBeanDao.Properties.Uid.gt(5),
+                        StudentBeanDao.Properties.Uid.le(50))).list();
+        for (int i = 0; i < beans.size(); i ++) {
+            log("index " + i + ": " + beans.get(i).toString());
+        }
+    }
 
+    /**
+     * 取5条Uid大于2的数据，且偏移2条
+     * @param v
+     */
+    public void onTest15(View v) {
+        QueryBuilder qb = StudentUtils.queryBuilder();
+        List<StudentBean> beans = qb.where(StudentBeanDao.Properties.Uid.gt(2)).limit(5).offset(2).list();
+        for (int i = 0; i < beans.size(); i ++) {
+            log("index " + i + ": " + beans.get(i).toString());
+        }
+    }
 
+    /**
+     * 多次执行查找
+     * @param v
+     */
+    public void onTest16(View v) {
+        QueryBuilder qb = StudentUtils.queryBuilder();
+        //搜索条件为Uid值大于1，即结果为[1,2,3,4,5];
+        // offset(2)表示往后偏移2个，结果为[3,4,5,6,7];
+        Query<StudentBean> query = qb.where(StudentBeanDao.Properties.Uid.gt(0)).limit(5).offset(2).build();
+        List<StudentBean> list = query.list();
+        for (int i = 0; i < list.size(); i ++) {
+            log("index " + i + ": " + list.get(i).toString());
+        }
+        //通过SetParameter来修改上面的查询条件，比如我们将上面条件修改取5条Id值大于2，往后偏移两位的数据，方法如下！
+        // setParameter 第一个表示条件的索引，在上边的条件是uid大于0，现在改成2，这个索引不能设置超过本来的数值，
+        // 例如设置成1会报错，IllegalArgumentException: Illegal parameter index: 1，因为条件只有一个
+        query.setParameter(0,2);
+        List<StudentBean> list1 = query.list();
+        for (int i = 0; i < list1.size(); i ++) {
+            log("index " + i + ": " + list1.get(i).toString());
+        }
+    }
 
-
-
+    /**
+     * 使用QueryBuilder进行批量删除操作
+     * 删除数据库中Uid大于5的所有其他数据
+     * @param v
+     */
+    public void onTest17(View v) {
+        QueryBuilder qb = StudentUtils.queryBuilder();
+        QueryBuilder<StudentBean> where = qb.where(StudentBeanDao.Properties.Uid.gt(5));
+        DeleteQuery<StudentBean> deleteQuery = where.buildDelete();
+        deleteQuery.executeDeleteWithoutDetachingEntities();
+    }
 
     private void log(String str) {
         System.out.println("=====================> " + str);
