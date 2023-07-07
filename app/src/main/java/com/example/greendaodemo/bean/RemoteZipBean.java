e924599314a41f2319cad304fb56024a6ef507d4
282330332 <282330332@qq.com>
2023-07-07 10:33:23 +0800

    代码合并

--- app/src/main/java/com/example/greendaodemo/bean/RemoteZipBean.java
+++ app/src/main/java/com/example/greendaodemo/bean/RemoteZipBean.java
@@ -3,11 +3,14 @@ package com.example.greendaodemo.bean;
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Generated;
 import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.DaoException;
+import com.example.greendaodemo.dao.DaoSession;
+import com.example.greendaodemo.dao.RemoteZipBeanDao;
 
 /**
  * 远程资源数据
  */
-@Entity
+@Entity(active = true, nameInDb = "remote_zip_bean")
 public class RemoteZipBean {
 
     @Id
@@ -18,10 +21,16 @@ public class RemoteZipBean {
     private String zipName = "";    // zip文件标识
     private int zipVersion;   // 版本号
     private String gameVersion; // 百顺游戏需要用到这个作为版本号 例如：1.1.2
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+    /** Used for active entity operations. */
+    @Generated(hash = 1341919531)
+    private transient RemoteZipBeanDao myDao;
 
     @Generated(hash = 1192987848)
     public RemoteZipBean(long zid, long zipDownloadSize, long zipAllSize,
-                         String zipUrl, String zipName, int zipVersion, String gameVersion) {
+            String zipUrl, String zipName, int zipVersion, String gameVersion) {
         this.zid = zid;
         this.zipDownloadSize = zipDownloadSize;
         this.zipAllSize = zipAllSize;
@@ -47,7 +56,6 @@ public class RemoteZipBean {
                 ", gameVersion=" + gameVersion +
                 '}';
     }
-
 
     public long getZid() {
         return this.zid;
@@ -104,5 +112,49 @@ public class RemoteZipBean {
     public void setGameVersion(String gameVersion) {
         this.gameVersion = gameVersion;
     }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 1678956941)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getRemoteZipBeanDao() : null;
+    }
+    
 
 }
